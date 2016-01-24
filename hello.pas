e94c0fba1f2b34b5d5bbb20e4563afd39366bd11
xDinomode <xDinomode@users.noreply.github.com>
2016-01-23 18:34:31 -0600

    whoops

--- hello.pas
+++ hello.pas
@@ -1,16 +1,10 @@
 program Greetings;
 
 var name : string;
-var age : integer;
 
-BEGIN
-     age := 5;
-     if(age < 2) then
-          writeln('No its samller')
-     else if(age > 2) then
-          writeln('Yes its bigger')
-     else
-         writeln('null');
-
+begin
+     writeln('Enter your name');
+     readln(name);
+     writeln('Hello ', name);
      readln;
-END.
+end.
